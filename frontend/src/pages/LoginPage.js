a08acfd90de5efeb4bc6027da4041104188a5af3
babu <hiroto.moriyama7204@gmail.com>
2026-09-01 23:42:23 +0900

    ログインページのHTML作成

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,11 +1,28 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import logoImage from "../images/app-logo.png";
+import robotSignInImage from "../images/robot-signin.png";
 
 function LoginPage({ onLogin }) {
     return (
         <div>
-            <h2>不労所得製造機</h2>
-            <LoginForm onLogin={onLogin} />
+            <div>
+                <img src={logoImage} alt="不労所得製造機" />
+            </div>
+            <div>
+                <div>
+                    <h1>ログイン</h1>
+                    <LoginForm onLogin={onLogin} />
+                    <div>
+                        <span>アカウントをお持ちでない方は</span>
+                        <button>新規登録</button>
+                    </div>
+                </div>
+
+                <div>
+                    <img src={robotSignInImage} alt="ロボット" />
+                </div>
+            </div>
         </div>
     );
 }
