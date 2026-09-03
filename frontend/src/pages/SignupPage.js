05ae1fd55c3cc057d55b7ff85b7f39335dd2d27b
babu <hiroto.moriyama7204@gmail.com>
2026-09-04 01:34:32 +0900

    新規登録画面からログイン画面に遷移できるように

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -3,7 +3,7 @@ import SignupForm from "../components/SignupForm";
 import logoImage from "../images/app-logo.png";
 import robotSignUpImage from "../images/robot-signup.png";
 
-function SignupPage({ onSignup }) {
+function SignupPage({ onSignup, onMoveLogin }) {
     return (
         <div className="signup-page">
 
@@ -17,7 +17,7 @@ function SignupPage({ onSignup }) {
                     <SignupForm onSignup={onSignup} />
                     <div className="login-area">
                         <span>アカウントをお持ちの方は</span>
-                        <button className="login-button">
+                        <button className="login-button" onClick={onMoveLogin}>
                             ログイン
                         </button>
                     </div>
